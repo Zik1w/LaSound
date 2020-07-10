eeecdea2cb5d70d2c60789e0d3c3dc612dfbc20d
Ziqi Wang <wzqmax@gmail.com>
2020-07-10 09:18:55 -0700

    LaSound plugin setup

--- Source/LaSoundTest/LaSoundTest.Build.cs
+++ Source/LaSoundTest/LaSoundTest.Build.cs
@@ -1,16 +1,16 @@
 //
-// <#Module Name#>.Build.cs
+// LaSoundTest.Build.cs
 //
-//  Generated on <#generation date#>
+//  Generated on July 10 2020
 //  Template created by Peter Gusev on 27 January 2020.
 //  Copyright 2013-2019 Regents of the University of California
 //
 
 using UnrealBuildTool;
 
-public class <#Module Name#> : ModuleRules
+public class LaSoundTest : ModuleRules
 {
-	public <#Module Name#>(ReadOnlyTargetRules Target) : base(Target)
+	public LaSoundTest(ReadOnlyTargetRules Target) : base(Target)
 	{
 		PCHUsage = ModuleRules.PCHUsageMode.UseExplicitOrSharedPCHs;
 
@@ -33,6 +33,7 @@ public class <#Module Name#> : ModuleRules
 			{
 				"Core",
 				"UMG",
+				"LaSound",
 				"DDBase",
 				"DDLog"
 				// ... add other public dependencies that you statically link with here ...
@@ -47,7 +48,7 @@ public class <#Module Name#> : ModuleRules
 				"Engine",
 				"Slate",
 				"SlateCore",
-				"deps<#Module Name#>"
+				"depsLaSound"
 				// ... add private dependencies that you statically link with here ...
 			}
 			);
